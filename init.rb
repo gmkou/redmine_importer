55ffde6c0efab8682e825f74eac51fcea55dd44d
Kosuke Nagano <gm_kou@mac.com>
2012-08-21 00:41:05 +0900

    fix for error: uninitialized constant ActionDispatch::Routing::Routes

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_importer do
   end
   menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
 
-  ActionController::Routing::Routes.draw do |map|
+  Rails.application.routes.draw do |map|
     map.connect 'projects/:project_id/importer/:action', :controller => 'importer'
   end
 end
